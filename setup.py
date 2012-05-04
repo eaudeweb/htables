b0834a3166e8049475a4d6e0c235f636f0df9820
Alex Morega <alex@grep.ro>
2012-05-04 14:25:34 +0300

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import distutils.core
 
 distutils.core.setup(
     name='htables',
-    version='0.1',
+    version='0.2',
     author='Eau de Web',
     author_email='office@eaudeweb.ro',
     py_modules=['htables'],
