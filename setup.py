3166a23b8b3a95e84f0fdc38003b633b685a168c
Alex Morega <alex@grep.ro>
2012-06-23 13:19:03 +0300

    set version to 0.3-rc1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import distutils.core
 
 distutils.core.setup(
     name='htables',
-    version='0.2.1',
+    version='0.3-rc1',
     author='Eau de Web',
     author_email='office@eaudeweb.ro',
     py_modules=['htables'],
