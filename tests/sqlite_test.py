d8fa5f5c92e1571febcdd8aea32b2acac884be43
Alex Morega <alex@grep.ro>
2012-07-17 11:49:28 +0300

    use multiple connections for filesystem database

--- tests/sqlite_test.py
+++ tests/sqlite_test.py
@@ -2,19 +2,29 @@ from __future__ import with_statement
 import unittest2 as unittest
 from contextlib import contextmanager
 import sqlite3
+import tempfile
 import simplejson as json
+from path import path
 from api_spec import _HTablesApiTest, create_schema
 
 
 class SqliteDB(object):
 
     def __init__(self, uri, schema):
-        self._single_connection = sqlite3.connect(uri)
+        if uri == ':memory:':
+            self._single_connection = sqlite3.connect(uri)
+        else:
+            self._single_connection = None
+            self._uri = uri
+
         self._files = {}
         self.schema = schema
 
     def _connect(self):
-        return self._single_connection
+        if self._single_connection is None:
+            return sqlite3.connect(self._uri)
+        else:
+            return self._single_connection
 
     def get_session(self):
         import htables
@@ -74,3 +84,21 @@ class SqliteSessionTest(unittest.TestCase):
     def test_memory_consecutive_access(self):
         db = SqliteDB(':memory:', schema=self.schema)
         self.assert_consecutive_sessions_access_same_database(db)
+
+    def create_filesystem_db(self):
+        self.tmp = path(tempfile.mkdtemp())
+        self.addCleanup(self.tmp.rmtree)
+        return SqliteDB(self.tmp / 'db.sqlite', schema=self.schema)
+
+    def test_filesystem_consecutive_access(self):
+        db = self.create_filesystem_db()
+        self.assert_consecutive_sessions_access_same_database(db)
+
+    def test_filesystem_different_connections(self):
+        db = self.create_filesystem_db()
+        with db_session(db) as session1:
+            session1.create_all()
+            session1.commit()
+            with db_session(db) as session2:
+                session1['person'].new(name="Joe")
+                self.assertEqual(list(session2['person'].find()), [])
