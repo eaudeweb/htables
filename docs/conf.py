9c49ce195e35b1429f1d4ec3b21a92b042b1ae1a
Alex Morega <alex@grep.ro>
2012-09-02 08:57:08 +0300

    autodiscover version; add repo to sys.path

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, re
 
 extensions = ['sphinx.ext.autodoc']
 templates_path = ['_templates']
@@ -6,8 +6,17 @@ source_suffix = '.rst'
 master_doc = 'index'
 project = u'HTables'
 copyright = u'2012, Alex Morega'
-version = release = '0.3'
 exclude_patterns = ['_build']
 pygments_style = 'sphinx'
 html_theme = 'default'
 htmlhelp_basename = 'HTablesdoc'
+
+_repo = os.path.join(os.path.dirname(__file__), '..')
+with open(os.path.join(_repo, 'setup.py'), 'rb') as f:
+    _line = [l for l in f if 'version' in l][0]
+version = release = re.match(r"\s*version\s*=\s*'([^']+)'.*", _line).group(1)
+
+try:
+    import htables
+except ImportError:
+    sys.path.append(_repo)
